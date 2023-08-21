ed5b7ff0dcb449ccb1ae9fae7d16fbb5ea15a736
niuliangtao <niuliangtao@qq.com>
2023-08-21 17:12:24 +0800

    add

--- setup.py
+++ setup.py
@@ -1,18 +1,12 @@
 import sys
-from os import path
-
-from notebuild.tool import read_version
+import time
 from setuptools import find_packages, setup
-
-version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
-
-version = read_version(version_path)
 
 install_requires = ['cryptography', 'notebuild']
 
-setup(name='notetool',
-      version=version,
-      description='notetool',
+setup(name='funtool',
+      version=time.strftime("%Y%m%d%H%M", time.localtime()),
+      description='funtool',
       author='niuliangtao',
       author_email='1007530194@qq.com',
       url='https://github.com/1007530194',
